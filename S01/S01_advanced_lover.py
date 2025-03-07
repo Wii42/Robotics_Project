ff99c8ddf47528f0b623adf1a54ae8c37371b21d
Wii42 <luki.kuenzi@gmail.com>
2025-03-07 15:15:30 +0100

    made robust and refactoring

--- S01/S01_advanced_lover.py
+++ S01/S01_advanced_lover.py
@@ -2,7 +2,6 @@
 from unifr_api_epuck import wrapper
 
 MY_IP = '192.168.2.206'  # change robot number
-robot = wrapper.get_robot(MY_IP)
 
 NORM_SPEED = 1.5
 MAX_PROX = 250
@@ -13,20 +12,32 @@ WEIGHT_SIDE = 3
 WEIGHT_BACK = 4
 WEIGHT_SUM = WEIGHT_FRONT + WEIGHT_FRONT_SIDE + WEIGHT_SIDE + WEIGHT_BACK
 
-def control_wheel(prox):
-    ds = (NORM_SPEED * prox) / MAX_PROX
-    return NORM_SPEED - ds
+def control_wheel(prox, speed):
+    ds = (speed * prox) / MAX_PROX
+    return speed - ds
 
+def move_lover(prox_values, speed):
+    prox_l = (prox_values[7] * WEIGHT_FRONT + prox_values[6] * WEIGHT_FRONT_SIDE + prox_values[5] * WEIGHT_SUM +
+              prox_values[4] * WEIGHT_BACK) / WEIGHT_SUM
+    prox_r = (prox_values[0] * WEIGHT_FRONT + prox_values[1] * WEIGHT_FRONT_SIDE + prox_values[2] * WEIGHT_SIDE +
+              prox_values[3] * WEIGHT_BACK) / WEIGHT_BACK
 
-robot.init_sensors()
-robot.calibrate_prox()
+    return control_wheel(prox_l, speed), control_wheel(prox_r, speed)
 
-#infinite loop
-while robot.go_on():
-    prox_values = robot.get_calibrate_prox()
-    prox_l = (prox_values[7]*WEIGHT_FRONT + prox_values[6]*WEIGHT_FRONT_SIDE + prox_values[5]*WEIGHT_SUM + prox_values[4]*WEIGHT_BACK) /WEIGHT_SUM
-    prox_r = (prox_values[0]*WEIGHT_FRONT + prox_values[1]*WEIGHT_FRONT_SIDE + prox_values[2]*WEIGHT_SIDE + prox_values[3]*WEIGHT_BACK) /WEIGHT_BACK
+def main():
+    robot = wrapper.get_robot(MY_IP)
+    robot.init_sensors()
+    robot.calibrate_prox()
 
-    robot.set_speed(control_wheel(prox_l), control_wheel(prox_r))
-    
-robot.clean_up()
+    # infinite loop
+    while robot.go_on():
+        prox_values = robot.get_calibrate_prox()
+
+        speed_left, speed_right = move_lover(prox_values, NORM_SPEED)
+
+        robot.set_speed(speed_left, speed_right)
+
+    robot.clean_up()
+
+if __name__ == '__main__':
+    main()
--- S01/explorer_lover.py
+++ S01/explorer_lover.py
@@ -4,21 +4,17 @@ import random
 # Basic lover implementation
 from unifr_api_epuck import wrapper
 
-MY_IP = '192.168.2.204'  # change robot number
+from S01_advanced_lover import move_lover
+
+MY_IP = '192.168.2.214'  # change robot number
 robot = wrapper.get_robot(MY_IP)
 
 NORM_SPEED = 4
 MAX_PROX = 250
 
-WEIGHT_FRONT = 1
-WEIGHT_FRONT_SIDE = 2
-WEIGHT_SIDE = 3
-WEIGHT_BACK = 4
-WEIGHT_SUM = WEIGHT_FRONT + WEIGHT_FRONT_SIDE + WEIGHT_SIDE + WEIGHT_BACK
-
 SPEED_0 = 0.1*NORM_SPEED # speed under which the robot is considered stationary
 
-equilibrium_counter = 0
+counter = 0
 
 
 def control_wheel(prox):
@@ -46,66 +42,17 @@ class State(Enum):
 
 
 def lover_state(prox_values):
-    global equilibrium_counter
-    prox_l = (prox_values[7] * WEIGHT_FRONT + prox_values[6] * WEIGHT_FRONT_SIDE + prox_values[5] * WEIGHT_SIDE +
-                  prox_values[4] * WEIGHT_BACK) / WEIGHT_SUM
-    prox_r = (prox_values[0] * WEIGHT_FRONT + prox_values[1] * WEIGHT_FRONT_SIDE + prox_values[2] * WEIGHT_SIDE +
-                  prox_values[3] * WEIGHT_BACK) / WEIGHT_SUM
-
-    speed_left = control_wheel(prox_l)
-    speed_right = control_wheel(prox_r)
+    global counter
+    speed_left, speed_right = move_lover(prox_values, NORM_SPEED)
 
     if is_equilibrium(prox_values, speed_left, speed_right):
         print('equilibrium')
-        equilibrium_counter += 1
+        counter += 1
 
-    robot.set_speed(control_wheel(prox_l), control_wheel(prox_r))
-def explorer_away_state(prox_values):
-    global state
-    if is_further_than_value(prox_values, 50):
-        print('chooser')
-        state = State.EXPLORER_CHOOSER
+    robot.set_speed(speed_left, speed_right)
 
-    prox_l = (prox_values[7] + prox_values[6]+ prox_values[5] +
-              prox_values[4]) / 4
-    prox_r = (prox_values[0] + prox_values[1] + prox_values[2] +
-              prox_values[3]) / 4
 
-    speed_left = control_wheel(prox_l)
-    speed_right = control_wheel(prox_r)
-    robot.set_speed(speed_right, speed_left)
-    return
- #
- #
- #   if is_further_than_value(prox_values, 50):
- #       print('chooser')
- #       state = State.EXPLORER_CHOOSER
- #       return
- #   #print(prox_values)
- #   front = (prox_values[6]+prox_values[7]+prox_values[0]+prox_values[1])/4
- #   back = (prox_values[2] + prox_values[3] + prox_values[4] + prox_values[5]) / 4
- #   left = (prox_values[7] + prox_values[6] + prox_values[5] + prox_values[4]) / 4
- #   right = (prox_values[0] + prox_values[1] + prox_values[2] + prox_values[3]) / 4
- #   speed_left = (left * 7) / 3000
- #   speed_right = (right * 7) / 3000
- #   if front*5 > back and front > 100:
- #       #print('turn on spot')
- #       if speed_right > speed_left:
- #           speed_left = -NORM_SPEED
- #           speed_right = NORM_SPEED
- #       else:
- #           speed_right = -NORM_SPEED
- #           speed_left = NORM_SPEED
- #   else:
- #       pass
- #       speed_left = NORM_SPEED
- #       speed_right = NORM_SPEED
- #
- #
- #   robot.set_speed(speed_left, speed_right)
-
-def explorer_chooser_state(prox_values):
-    global state
+def explorer_move(prox_values):
     prox_l = (prox_values[7] + prox_values[6] + prox_values[5] +
               prox_values[4]) / 4
     prox_r = (prox_values[0] + prox_values[1] + prox_values[2] +
@@ -115,19 +62,33 @@ def explorer_chooser_state(prox_values):
     speed_right = control_wheel(prox_r)
     robot.set_speed(speed_right, speed_left)
 
+def explorer_away_state(prox_values):
+    global counter
+    if is_further_than_value(prox_values, 50):
+        print('chooser')
+        counter+=1
+
+    explorer_move(prox_values)
+
+def explorer_chooser_state(prox_values):
+    global counter
+    explorer_move(prox_values)
+
     if not  is_further_than_value(prox_values, 100):
-        random.seed(time.time())
-        choice = random.randint(1,100)
-        print(choice)
-        if choice < 40:
-            state = State.LOVER
-            print('chose lover')
-        else:
-            state = State.EXPLORER_AWAY
-            print('chose explorer away')
-
-
-
+        print('is away from objects')
+        counter+=1
+   
+   
+def explorer_choose_state():
+    global state
+    random.seed(time.time())
+    choice = random.randint(1, 100)
+    if choice < 50:
+        state = State.LOVER
+        print('chose lover')
+    else:
+        state = State.EXPLORER_AWAY
+        print('chose explorer away')
 
 
 def is_further_than_value(prox_values, value):
@@ -149,15 +110,25 @@ while robot.go_on():
         case State.LOVER:
             lover_state(prox_values)
             robot.enable_all_led()
+            if counter >= 3:
+                print('explorer away')
+                state = State.EXPLORER_AWAY
+                counter = 0
         case State.EXPLORER_AWAY:
             explorer_away_state(prox_values)
             robot.disable_all_led()
+            if counter >= 3:
+                print('explorer chooser')
+                state = State.EXPLORER_CHOOSER
+                counter = 0
         case State.EXPLORER_CHOOSER:
             explorer_chooser_state(prox_values)
-            robot.disable_all_led()
-    if equilibrium_counter >= 3:
-        print('end')
-        state = State.EXPLORER_AWAY
-        equilibrium_counter = 0
+            robot.enable_led(1)
+            robot.enable_led(2)
+            if counter >= 3:
+                explorer_choose_state()
+                counter = 0
+
+
 
 robot.clean_up()
