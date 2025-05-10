e7657aba2311669dc25c36a7738cbbb8fc2e67a2
Wii42 <luki.kuenzi@gmail.com>
2025-05-10 15:34:07 +0200

    refactoered robot_controller

--- project2/robot/robot_controller.py
+++ project2/robot/robot_controller.py
@@ -1,3 +1,4 @@
+import json
 import sys, signal
 
 from unifr_api_epuck import wrapper
@@ -18,73 +19,109 @@ LINE_MAX: int = 750  # to determine if the sensor is on the line
 GREY_MIN: int = 450  # to determine if the sensor is on the grey area
 
 
-def send_pos(robot: WifiEpuck, robot_position: list[float], position_on_track: PositionOnTrack):
-    robot.ClientCommunication.send_msg_to(coordinator.COORDINATOR_ID,
-                                          {"robot_id": robot.id, "robot_position": robot_position.copy(),
-                                           "position_on_track": position_on_track.to_dict()})
+class RobotController:
+    def __init__(self, robot_ip: str, norm_speed: float = 1):
+        self.norm_speed = norm_speed
+        self.robot = wrapper.get_robot(robot_ip)
+
+        self.step_counter: StepCounter = None
+        self.proximity_memory: SensorMemory = None
+        self.grey_area: GreyArea = None
+        self.obstacle_avoider: ObstacleAvoider = None
+        self.beacon_detector: BeaconDetector = None
+        self.track_follower: TrackFollower = None
+        self.odometry: Odometry = None
+
+        def handler(signum, frame):
+            self.robot.clean_up()
+
+        signal.signal(signal.SIGINT, handler)
+
+    def run(self):
+        self.init_track_follower_odometry()
+
+        self.robot.calibrate_prox()
+        self.robot.init_client_communication()
+
+        self.proximity_memory: SensorMemory = SensorMemory(5)
+        self.obstacle_avoider: ObstacleAvoider = ObstacleAvoider(40, 100)
+
+        while self.robot.go_on():
+            gs: list[int] = self.robot.get_ground()
+            self.read_proximity_sensors()
+
+            self.handle_incoming_messages()
+            self.odometry.odometry(*self.track_follower.current_speed)
+
+            self.beacon_detector.receive_ground(gs)
+            self.check_for_beacons()
+
+            self.notify_coordinator_of_position()
+
+            self.adjust_speed_to_possible_obstacle()
+
+            if not self.track_follower.follow_track(gs):
+                break
+
+            self.step_counter.step()
+
+        self.robot.clean_up()
+
+    def init_track_follower_odometry(self):
+        self.robot.init_ground()
+        self.step_counter: StepCounter = StepCounter()
+        self.grey_area: GreyArea = GreyArea(self.norm_speed)
+        self.beacon_detector: BeaconDetector = BeaconDetector(self.norm_speed, self.grey_area, GREY_MIN, LINE_MAX,
+                                                              coordinator.BEACONS,
+                                                              self.step_counter)
+        self.track_follower: TrackFollower = TrackFollower(self.robot, self.norm_speed, LINE_MAX)
+        self.odometry: Odometry = Odometry(self.robot, self.step_counter)
+
+    def adjust_speed_to_possible_obstacle(self):
+        self.track_follower.obstacle_speed_factor = self.obstacle_avoider.calc_speed(
+            self.proximity_memory.get_average())
+        if self.track_follower.obstacle_speed_factor != 1.0:
+            print(
+                f"[{self.robot.id.split('_')[-1]}] obstacle speed factor: {self.track_follower.obstacle_speed_factor}")
+
+    def notify_coordinator_of_position(self):
+        if self.step_counter.get_steps() % 5 == 0:
+            self.send_pos([self.odometry.x, self.odometry.y], self.odometry.position_from_beacon)
+
+    def send_pos(self, robot_position: list[float], position_on_track: PositionOnTrack):
+        self.robot.ClientCommunication.send_msg_to(coordinator.COORDINATOR_ID,
+                                              {"robot_id": self.robot.id, "robot_position": robot_position.copy(),
+                                               "position_on_track": position_on_track.to_dict()})
+
+    def check_for_beacons(self):
+        if self.beacon_detector.new_beacon_found():
+            print(f"[{self.robot.id.split('_')[-1]}] found beacon: {self.beacon_detector.last_beacon.name}")
+            if self.odometry.position_from_beacon.from_beacon is not None:
+                calibrate_robot(self.robot.id,
+                                f"[{self.odometry.position_from_beacon.from_beacon.name}->{self.beacon_detector.last_beacon.name}] {self.odometry.position_from_beacon.distance}")
+            self.odometry.sync_with_beacon(self.beacon_detector.last_beacon)
+
+    def read_proximity_sensors(self):
+        self.proximity_memory.update_memory(self.robot.get_calibrate_prox())
+
+    def handle_incoming_messages(self):
+        while self.robot.has_receive_msg():
+            msg = self.robot.receive_msg()
+            if msg.get("speed_factor"):
+                speed_factor = msg.get("speed_factor")
+                print(f"[{self.robot.id.split('_')[-1]}] received speed factor: {speed_factor}")
+                self.track_follower.speed_factor = speed_factor
+
+
+
 def calibrate_robot(robot_id: str, msg: str):
+    # with open("calibrate.json", "r") as f:
+    #    string = f.read()
+    #    calibration_dict = json.loads(string)
     with open("calibrate.json", "a") as f:
         f.write(f"{robot_id}: {msg}\n")
         f.flush()
         f.close()
-
-def main(robot_ip: str, norm_speed: float = 1):
-    robot = wrapper.get_robot(robot_ip)
-
-    def handler(signum, frame):
-        robot.clean_up()
-
-    signal.signal(signal.SIGINT, handler)
-
-    robot.init_ground()
-    robot.calibrate_prox()
-    robot.init_client_communication()
-
-    step_counter: StepCounter = StepCounter()
-    proximity_memory: SensorMemory = SensorMemory(5)
-    grey_area: GreyArea = GreyArea(norm_speed)
-    obstacle_avoider: ObstacleAvoider = ObstacleAvoider(40, 100)
-    detector: BeaconDetector = BeaconDetector(norm_speed, grey_area, GREY_MIN, LINE_MAX, coordinator.BEACONS,
-                                              step_counter)
-    track_follower: TrackFollower = TrackFollower(robot, norm_speed, LINE_MAX)
-    odometry: Odometry = Odometry(robot, step_counter)
-
-    while robot.go_on():
-        robot.go_on()
-        gs: list[int] = robot.get_ground()
-        proximity_memory.update_memory(robot.get_calibrate_prox())
-
-        while robot.has_receive_msg():
-            msg = robot.receive_msg()
-            if msg.get("speed_factor"):
-                speed_factor = msg.get("speed_factor")
-                print(f"[{robot.id.split('_')[-1]}] received speed factor: {speed_factor}")
-                track_follower.speed_factor = speed_factor
-        odometry.odometry(track_follower.current_speed[0], track_follower.current_speed[1])
-
-        detector.receive_ground(gs)
-
-        # odometry.print_position()
-
-        if detector.new_beacon_found():
-            print(f"[{robot.id.split('_')[-1]}] found beacon: {detector.last_beacon.name}")
-            if odometry.position_from_beacon.from_beacon is not None:
-                calibrate_robot(robot.id, f"[{odometry.position_from_beacon.from_beacon.name}->{detector.last_beacon.name}] {odometry.position_from_beacon.distance}")
-            odometry.sync_with_beacon(detector.last_beacon)
-
-        if step_counter.get_steps() % 5 == 0:
-            send_pos(robot, [odometry.x, odometry.y], odometry.position_from_beacon)
-
-        track_follower.obstacle_speed_factor = obstacle_avoider.calc_speed(proximity_memory.get_average())
-        if track_follower.obstacle_speed_factor != 1.0:
-            print(f"[{robot.id.split('_')[-1]}] obstacle speed factor: {track_follower.obstacle_speed_factor}")
-
-        if not track_follower.follow_track(gs):
-            break
-
-        step_counter.step()
-
-    robot.clean_up()
 
 
 if __name__ == '__main__':
@@ -92,4 +129,4 @@ if __name__ == '__main__':
         ip = sys.argv[1]
     else:
         ip = '192.168.2.207'
-    main(ip)
+    RobotController(ip).run()
