7c417173868fe8b6c486e8d347d12e7db7d779c5
Wii42 <luki.kuenzi@gmail.com>
2025-05-12 15:03:47 +0200

    refactored task2 to using classes

--- task_2/main.py
+++ task_2/main.py
@@ -1,175 +1,172 @@
+from enum import Enum
+
 from unifr_api_epuck import wrapper
 import os
 
 from unifr_api_epuck.epuck.epuck_wifi import WifiEpuck
 
+from project2.robot.sensor_memory import SensorMemory
 from project2.robot.step_counter import StepCounter
 from project2.robot.track_follower import TrackFollower
 from determine_side import TrackSide
 from determine_side import DetermineSide
-from ground_sensor_memory import GroundSensorMemory
 from line_alignment import LineAlignment
 import utils
-import time
 
 MY_IP = '192.168.2.214'
 LINE_MAX_VALUE: int = 500
 
 STEPS_TO_DETERMINE_SIDE: int = 20
 
+OBJECT_DETECTIONS_DIR = "./object_detections"
+
+
+class KartState(Enum):
+    LINE_FOLLOWING_AND_ALIGNMENT = (0, lambda mario_kart: mario_kart.line_following_and_alignment())
+    CHANGE_LANE_TO_LEFT = (1, lambda mario_kart: mario_kart.change_lane_to_left())
+    CHANGE_LANE_TO_RIGHT = (2, lambda mario_kart: mario_kart.change_lane_to_right())
+
+    def __init__(self, value, function):
+        self.value: int = value
+        self.function = function
+
+    def do(self, mario_kart: 'MarioKart'):
+        return self.function(mario_kart)
+
 
 class MarioKart:
 
-    def __init__(self, robot_ip: str):
+    def __init__(self, robot_ip: str, norm_speed: float = 3):
         self.robot_ip: str = robot_ip
-        self.robot: WifiEpuck | None = None # init later
+        self.norm_speed: float = norm_speed
+        self.robot: WifiEpuck | None = None  # init later
         self.counter: StepCounter = StepCounter()
-        self.line_follower: TrackFollower | None = None # dependent on robot
-        self.sensor_memory: GroundSensorMemory = GroundSensorMemory(3)
+        self.line_follower: TrackFollower | None = None  # dependent on robot
+        self.ground_sensor_memory: SensorMemory = SensorMemory(3)
+        self.determine_side: DetermineSide = DetermineSide(750, LINE_MAX_VALUE, STEPS_TO_DETERMINE_SIDE)
+        self.line_alignment: LineAlignment = LineAlignment()
+        self.check_side_necessary: bool = True
+        self.current_state: KartState = KartState.LINE_FOLLOWING_AND_ALIGNMENT
+        self.step_of_last_detection = 0
+        self.detected_lines = 0
 
+    def init_robot(self):
+        self.robot = wrapper.get_robot(MY_IP)
+        try:
+            os.mkdir(OBJECT_DETECTIONS_DIR)
+        except OSError as error:
+            print(error)
+        self.robot.init_ground()
 
+        self.robot.initiate_model()
 
-def main():
-    dir = "./object_detections"
-    robot = wrapper.get_robot(MY_IP)
-    try:
-        os.mkdir(dir)
-    except OSError as error:
-        print(error)
-    robot.init_ground()
+        self.robot.init_sensors()
+        self.robot.calibrate_prox()
 
-    robot.initiate_model()
+    def init_line_follower(self):
+        """
+        Initialize the line follower with the robot and the max for the ground be considered black.
+        Should only be called after the robot is initialized by self.init_robot().
+        :return:
+        """
+        self.line_follower: TrackFollower = TrackFollower(self.robot, self.norm_speed, LINE_MAX_VALUE)
 
-    robot.init_sensors()
-    robot.calibrate_prox()
+    def line_following_and_alignment(self):
 
-    counter: StepCounter = StepCounter()
-
-    line_follower: TrackFollower = TrackFollower(robot, 3, LINE_MAX_VALUE)
-
-    sensor_memory: GroundSensorMemory = GroundSensorMemory(3)
-
-    determine_side: DetermineSide = DetermineSide(750, LINE_MAX_VALUE, STEPS_TO_DETERMINE_SIDE)
-
-    line_alignment: LineAlignment = LineAlignment()
-
-    check_side_necessary: bool = True
-
-    ####################################
-    no_error = True
-    curr_state = 0
-
-    def line_following_and_alignment():
-
-        nonlocal check_side_necessary
-        nonlocal curr_state
-        if counter.get_steps() > STEPS_TO_DETERMINE_SIDE and check_side_necessary:
-            line_alignment.check_line_alignment(determine_side.get_probable_side())
+        if self.counter.get_steps() > STEPS_TO_DETERMINE_SIDE and self.check_side_necessary:
+            self.line_alignment.check_line_alignment(self.determine_side.get_probable_side())
             check_side_necessary = False
 
         # print(gs)
         # print(gs)
         # robot.set_speed(5,5)
-        if not line_follower.follow_track(sensor_memory.get_average(), use_two_sensors_approach=True,
-                                          invert_side=line_alignment.get_follow_left_side()):
+        if not self.line_follower.follow_track(self.ground_sensor_memory.get_average(), use_two_sensors_approach=True,
+                                               invert_side=self.line_alignment.get_follow_left_side()):
             return False
-        determine_side.determine_side(sensor_memory.get_average(), line_follower.position,
-                                      invert_side=line_alignment.get_follow_left_side())
+        self.determine_side.determine_side(self.ground_sensor_memory.get_average(), self.line_follower.position,
+                                           invert_side=self.line_alignment.get_follow_left_side())
         # print(determine_side.get_probable_side())
         # print()
-        curr_state = change_lanes_detection()
+        self.current_state = self.change_lanes_detection()
         return True
 
-    step_of_last_detection = 0
-    stand_speed = 2
+    def change_lanes_detection(self):
 
-    def change_to_line_following():
-        nonlocal curr_state
-        curr_state = 0
+        if self.counter.get_steps() % 50 == 0:
+            self.robot.init_camera(dir)
 
-    def change_lane_to_right():
+        curr_block = None
+        if self.counter.get_steps() % 50 == 1:
+            curr_block = utils.block_detector(self.robot, 30, 20)
+            self.robot.disable_camera()
+        if curr_block is not None:
+            currSide = self.determine_side.get_probable_side()
 
-        robot.set_speed(stand_speed * 1.2, stand_speed * 0.9)
-        if line_detection():
-            change_to_line_following()
+            if (currSide == TrackSide.LEFT) and (curr_block == "Green Block"):
+                self.step_of_last_detection = self.counter.get_steps()
+                return 2
+            if (currSide == TrackSide.RIGHT) and (curr_block == "Red Block"):
+                self.step_of_last_detection = self.counter.get_steps()
+                return 1
+        return 0
+
+    def change_lane_to_right(self):
+
+        self.robot.set_speed(self.norm_speed * 1.2, self.norm_speed * 0.9)
+        if self.line_detection():
+            self.current_state = KartState.LINE_FOLLOWING_AND_ALIGNMENT
         return True
 
-    def change_lane_to_left():
+    def change_lane_to_left(self):
 
-        nonlocal curr_state
-        robot.set_speed(stand_speed * 0.9, stand_speed * 1.2)
-        if line_detection():
-            change_to_line_following()
+        self.robot.set_speed(self.norm_speed * 0.9, self.norm_speed * 1.2)
+        if self.line_detection():
+            self.current_state = KartState.LINE_FOLLOWING_AND_ALIGNMENT
         return True
 
-    detected_lines = 0
+    def line_detection(self):
 
-    def line_detection():
-        nonlocal detected_lines
-
-        if counter.get_steps() - step_of_last_detection > 200:
-            detections = sensor_memory.get_average()
+        if self.counter.get_steps() - self.step_of_last_detection > 200:
+            detections = self.ground_sensor_memory.get_average()
             temp = 0
             for detec in detections:
                 if detec < LINE_MAX_VALUE:
-                    detected_lines += 1
-            if detected_lines > 10:
+                    self.detected_lines += 1
+            if self.detected_lines > 10:
                 detected_lines = 0
                 return True
             return False
         return None
 
-    def change_lanes_detection():
-        nonlocal step_of_last_detection
-
-        if counter.get_steps() % 50 == 0:
-            robot.init_camera(dir)
-
-        curr_block = None
-        if counter.get_steps() % 50 == 1:
-            curr_block = utils.block_detector(robot, 30, 20)
-            robot.disable_camera()
-        if curr_block is not None:
-            currSide = determine_side.get_probable_side()
-
-            if (currSide == TrackSide.LEFT) and (curr_block == "Green Block"):
-                step_of_last_detection = counter.get_steps()
-                return 2
-            if (currSide == TrackSide.RIGHT) and (curr_block == "Red Block"):
-                step_of_last_detection = counter.get_steps()
-                return 1
-        return 0
-
     ####################################################################################################
 
-    def detect_epucks():
-        prox_values = robot.get_calibrate_prox()
+    def detect_epucks(self):
+        prox_values = self.robot.get_calibrate_prox()
         av_front_prox = (prox_values[6] * 2 + prox_values[7] + prox_values[0] + prox_values[1] * 2) / 4
 
         if av_front_prox > 100:
             return True
         return False
 
-    def detect_end():
+    def detect_end(self):
 
-        distance = robot.get_tof()
+        distance = self.robot.get_tof()
         if distance <= 50:
             return True
         return False
 
-    states = {0: line_following_and_alignment,
-              1: change_lane_to_left,
-              2: change_lane_to_right}
+    def run(self):
+        no_error = True
 
-    while robot.go_on() and no_error:
-        gs: list[int] = robot.get_ground()
-        sensor_memory.update_memory(gs)
-        no_error = states[curr_state]()
+        while self.robot.go_on() and no_error:
+            gs: list[int] = self.robot.get_ground()
+            self.ground_sensor_memory.update_memory(gs)
+            no_error = self.current_state.do(self)
 
-        counter.step()
+            self.counter.step()
 
-    robot.clean_up()
+        self.robot.clean_up()
 
 
 if __name__ == "__main__":
-    main()
+    MarioKart(MY_IP, norm_speed=3).run()
